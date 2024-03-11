50c4a60d199093749067ca178161e3efeb9d81cc
Softkingz <knalexkingsley@gmail.com>
2024-03-11 18:25:03 +0000

    Python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,17 +7,21 @@
  * Return: 0 if there is no cycle,
  * 1 if there is a cycle
  */
-int check_cycle(listint_t *list) 
-{
+int check_cycle(listint_t *list) {
     listint_t *slow_ptr = list;
     listint_t *fast_ptr = list;
 
-     while (slow_ptr && fast_ptr && fast_ptr->next) {
+    while (slow_ptr && fast_ptr && fast_ptr->next) {
         slow_ptr = slow_ptr->next;
         fast_ptr = fast_ptr->next->next;
 
         if (slow_ptr == fast_ptr) {
-            return 1;  // Cycle detected
+            slow_ptr = list;
+            while (slow_ptr != fast_ptr) {
+                slow_ptr = slow_ptr->next;
+                fast_ptr = fast_ptr->next;
+            }
+            return (1);  // Cycle detected
         }
     }
 
