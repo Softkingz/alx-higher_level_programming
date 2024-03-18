1487dc9a0b869487a6d8ddaf1eb1caddeb1da3f6
Softkingz <knalexkingsley@gmail.com>
2024-03-18 17:46:40 +0000

    python-data_structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    """Print all integers of a list."""
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def element_at(my_list, idx):
+    """Retrieve an element from a list."""
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
